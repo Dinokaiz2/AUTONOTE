506706865244a0b117a98b92067e0f99b7126d06
Dinokaiz2 <kakhtar@rthighschool.org>
2016-10-04 21:50:43 -0400

    First Commit, right now have a basic snip tool, no image to text.

---
+++ AUTONOTE/StartForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AUTONOTE
+{
+    public partial class StartForm : Form
+    {
+        public StartForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var bmp = SnipForm.Snip();
+            ImageForm imageForm = new ImageForm(bmp);
+            imageForm.Show(this);
+        }
+    }
+}
